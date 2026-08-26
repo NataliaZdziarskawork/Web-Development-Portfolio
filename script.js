9c631679bfc8436ea1ccda850e264f7211d0f137
Natalia <313094997+NataliaZdziarskawork@users.noreply.github.com>
2026-08-26 10:03:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const przycisk = document.querySelector('#start a');
 const wiadomosc = document.querySelector('#wiadomosc');
 
 przycisk.addEventListener('click', function() {
-    wiadomosc.textContent = 'Dziękuję za kontakt! Chętnie podejmę się realizacji Twojego projektu.';
+    wiadomosc.textContent = 'Dziękuję za kontakt!';
+});
+
+const formularz = document.querySelector('#formularz-kontaktowy');
+const komunikat = document.querySelector('#komunikat-formularza');
+
+formularz.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    komunikat.textContent = 'Dziękuję za wiadomość! Skontaktuję się z Tobą.';
+    formularz.reset();
 });
